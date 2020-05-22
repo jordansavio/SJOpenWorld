c5ce006243e43a2d458555dbe2d86b53332b2bd9
Jordan Savio <jordan.savio.95@gmail.com>
2020-05-22 16:41:38 -0300

    2.5 - W5 - Random

---
+++ Assets/Scripts/WeaponMulti.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMulti : MonoBehaviour
+{
+    public Transform[] T;
+    public float speed;
+    public GameObject Shot;
+    int index;
+
+    void Start()
+    {
+        index = Random.Range(0, 2);
+        Invoke("Fire", 0.5f);
+
+    }
+
+    void Update()
+    {
+        transform.position += Time.deltaTime * speed * transform.forward;
+    }
+
+    void Fire()
+    {
+        Instantiate(Shot, T[index].transform.position + transform.forward, T[index].transform.rotation);
+    }
+}
